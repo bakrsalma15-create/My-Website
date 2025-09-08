49f199b6635c401722c85250db258ac18e5935cf
maryamomar <maryam.omar@campus.tu-berlin.de>
2025-09-08 11:00:06 +0200

    new pages

--- maryamomar.com/public_html/script.js
+++ maryamomar.com/public_html/script.js
@@ -1,52 +1,32 @@
-// geting canvas by Boujjou Achraf
+
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
-
-//making the canvas full screen
 c.height = window.innerHeight;
 c.width = window.innerWidth;
 
-//chinese characters - taken from the unicode charset
-var matrix = "ا ب ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن هـ و ي@#$%^&*()*&^%+-/~{[|`]}";
-//converting the string into an array of single characters
+var matrix = "اً إً أً بً تً ثً جً حً خً دً ذً رً زً سً شً صً ضً طً ظً عً غً فً قً كً لً مً نً هـً وً يً" +
+             "ٍ إٍ أٍ بٍ تٍ ثٍ جٍ حٍ خٍ دٍ ذٍ رٍ زٍ سٍ شٍ صٍ ضٍ طٍ ظٍ عٍ غٍ فٍ قٍ كٍ لٍ مٍ نٍ هـٍ وٍ يٍ" +
+             "ٌ إٌ أٌ بٌ تٌ ثٌ جٌ حٌ خٌ دٌ ذٌ رٌ زٌ سٌ شٌ صٌ ضٌ طٌ ظٌ عٌ غٌ فٌ قٌ كٌ لٌ مٌ نٌ هـٌ وٌ يٌ" +
+             "@#$%^&*()*&^%+-/~{[|`]}";
 matrix = matrix.split("");
 
 var font_size = 10;
-var columns = c.width/font_size; //number of columns for the rain
-//an array of drops - one per column
+var columns = c.width / font_size;
 var drops = [];
-//x below is the x coordinate
-//1 = y co-ordinate of the drop(same for every drop initially)
-for(var x = 0; x < columns; x++)
-    drops[x] = 3; 
+for (var x = 0; x < columns; x++)
+    drops[x] = Math.floor(Math.random() * c.height / font_size);
 
-//drawing the characters
-function draw()
-{
-    //Black BG for the canvas
-    //translucent BG to show trail
+function draw() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
     ctx.fillRect(0, 0, c.width, c.height);
-
     ctx.fillStyle = "#8b8b8bff";
-    ctx.font = font_size + "px arial";
-    //looping over drops
-    for(var i = 0; i < drops.length; i++)
-    {
-        //a random chinese character to print
-        var text = matrix[Math.floor(Math.random()*matrix.length)];
-        //x = i*font_size, y = value of drops[i]*font_size
-        ctx.fillText(text, i*font_size, drops[i]*font_size);
-
-        //sending the drop back to the top randomly after it has crossed the screen
-        //adding a randomness to the reset to make the drops scattered on the Y axis
-        if(drops[i]*font_size > c.height && Math.random() > 0.975)
+    ctx.font = font_size + "px monospace";
+    for (var i = 0; i < drops.length; i++) {
+        var text = matrix[Math.floor(Math.random() * matrix.length)];
+        ctx.fillText(text, i * font_size, drops[i] * font_size);
+        if (drops[i] * font_size > c.height && Math.random() > 0.975)
             drops[i] = 0;
-
-        //incrementing Y coordinate
         drops[i]++;
     }
 }
-
 setInterval(draw, 35);
-
